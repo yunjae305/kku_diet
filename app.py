284acb9161f722be7bf3f652aa224911ce907094
Kim <kyj31590039@gmail.com>
2026-03-10 01:09:46 +0900

    Render 배포 설정 추가 (Procfile, gunicorn, PORT 환경변수)

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from crawler import get_today_meals, get_tomorrow_meals, get_week_meals
 from user_store import get_user_dorm, set_user_dorm
@@ -119,4 +120,5 @@ def settings_api():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
